538bd16a952de16e0d2e2e4fcbf98dee4b06eac4
Админ <nikita_rudakov_03@mail.ru>
2020-03-29 20:42:19 +0300

    ex4

--- program.py
+++ program.py
@@ -30,5 +30,41 @@ def picture():
            '<figcaption>Вот она какая, красная планета.</figcaption>'
 
 
+@app.route('/promotion_image')
+def bootstrap():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+                    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" href="/static/css/style.css" type="text/css">
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="/static/img/mars.png">
+                    <h5 class="alert alert-dark" role="alert">
+                        Человечество вырастает из детства.
+                    </h5>
+                    <h5 class="alert alert-success" role="alert">
+                        Человечеству мала одна планета.
+                    </h5>
+                    <h5 class="alert alert-secondary" role="alert">
+                        Мы сделаем обитаемыми безжизненные пока планеты.
+                    </h5>
+                    <h5 class="alert alert-warning" role="alert">
+                        И начнем с Марса!
+                    </h5>
+                    <h5 class="alert alert-danger" role="alert">
+                        Присоединяйся!
+                    </h5>
+                  </body>
+                </html>'''
+
+
 if '__main__' == __name__:
     app.run(port=8080, host='127.0.0.1')
